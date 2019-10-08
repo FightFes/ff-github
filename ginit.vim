fca7b2399da24c3fdeb87c1ee1e351d4cf62963a
FightFes <kinntaikun@gmail.com>
2019-10-08 15:48:04 +0900

    update link.sh delete gvimrc ginit.vim

--- ginit.vim
+++
@@ -1,7 +0,0 @@
-let s:init_file_dir = ''
-if has('mac')
-  let s:init_file_dir = '.vim'
-elseif has('win32') || has('win64')
-  let s:init_file_dir = 'vimfiles'
-endif
-execute('source ~/' . s:init_file_dir . '/gvimrc')
--- link.sh
+++ link.sh
@@ -4,9 +4,28 @@
 echo $0
 cd `dirname $0`
 
-rm -f ~/.vim/vimrc ~/.vim/gvimrc ~/.config/nvim/init.vim
+LINK_SOURCE_PATH_LIST=(~/ff-github/vim/vimrc
+                       ~/ff-github/vim/init.vim)
+LINK_DEST_PATH_LIST=(~/.vim/vimrc 
+                     ~/.config/nvim/init.vim) 
+
+for i in ${!LINK_SOURCE_PATH_LIST[@]}
+do
+  #ないならディレクトリ作成
+  mkdir -p -v $(dirname ${LINK_DEST_PATH_LIST[i]})
+  #今あるファイルを削除
+  rm -f ${LINK_DEST_PATH_LIST[i]}
+  #シンボリックリンクを貼る
+  sudo ln -s ${LINK_SOURCE_PATH_LIST[i]} ${LINK_DEST_PATH_LIST[i]}
+done
+
+# -fでファイルのみ、-dでディレクトリのみ、-eで両方
+# if [ ! -e ~/.vim ]
+# fi
+
+# rm -f ~/.vim/vimrc ~/.vim/gvimrc ~/.config/nvim/init.vim
 
 # 絶対パスで指定した方がいい
-sudo ln -s ~/ff-github/vimrc ~/.vim/vimrc
-sudo ln -s ~/ff-github/gvimrc ~/.vim/gvimrc
-sudo ln -s ~/ff-github/init.vim ~/.config/nvim/init.vim
+# sudo ln -s ~/ff-github/vimrc ~/.vim/vimrc
+# sudo ln -s ~/ff-github/gvimrc ~/.vim/gvimrc
+# sudo ln -s ~/ff-github/init.vim ~/.config/nvim/init.vim
