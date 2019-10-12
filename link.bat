fd54ffd8ebaaaa00857d7b5a561b87b8556ce2ad
FightFes <kinntaikun@gmail.com>
2019-10-12 19:24:53 +0900

    update vimrc

--- link.bat
+++ link.bat
@@ -10,13 +10,12 @@ rem   echo シンボリックリンクを作成
 rem   mklink ..\.hyper.js .\.hyper.js
 rem )
 
-call :makelink ..\.hyper.js %USERPROFILE%\ff-github\.hyper.js
-call :makelink ..\vimfiles\vimrc %USERPROFILE%\ff-github\vimrc
-call :makelink ..\vimfiles\gvimrc %USERPROFILE%\ff-github\gvimrc
-call :makelink ..\AppData\Local\nvim\init.vim %USERPROFILE%\ff-github\init.vim
+call :MakeLink ..\vimfiles\vimrc %USERPROFILE%\ff-github\vim\vimrc
+call :MakeLink ..\AppData\Local\nvim\init.vim %USERPROFILE%\ff-github\vim\init.vim
 exit /b
 
-:makelink
+:MakeLink
+md %~dp1
 del /Q %1
 mklink %1 %2
 exit /b
