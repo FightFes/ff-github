4c9ff232fe912c4130fd2150f1924547bea17b9e
FightFes <kinntaikun@gmail.com>
2019-11-07 17:43:38 +0900

    update link.sh

--- link.sh
+++ link.sh
@@ -5,16 +5,18 @@ echo $0
 cd `dirname $0`
 
 LINK_SOURCE_PATH_LIST=(~/ff-github/vim/vimrc
-                       ~/ff-github/vim/init.vim)
+                       ~/ff-github/vim/init.vim
+                       ~/ff-github/ranger)
 LINK_DEST_PATH_LIST=(~/.vim/vimrc 
-                     ~/.config/nvim/init.vim) 
+                     ~/.config/nvim/init.vim
+                     ~/.config/ranger) 
 
 for i in ${!LINK_SOURCE_PATH_LIST[@]}
 do
   #ないならディレクトリ作成
   mkdir -p -v $(dirname ${LINK_DEST_PATH_LIST[i]})
   #今あるファイルを削除
-  rm -f ${LINK_DEST_PATH_LIST[i]}
+  rm -f -r ${LINK_DEST_PATH_LIST[i]}
   #シンボリックリンクを貼る
   sudo ln -s ${LINK_SOURCE_PATH_LIST[i]} ${LINK_DEST_PATH_LIST[i]}
 done
