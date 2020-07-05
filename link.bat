4f5a14a70119f1aa6ac9d7564eeac81384ce11d7
kinntai <kinntaikun@gmail.com>
2020-07-06 00:57:46 +0900

    update config files

--- link.bat
+++ link.bat
@@ -8,6 +8,8 @@ call :MakeDirLink %USERPROFILE%\AppData\Local\nvim %~dp0nvim
 call :MakeLink %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %~dp0config\settings.json
 call :MakeLink %USERPROFILE%\afxw64_163\AFXW.KEY %~dp0config\AFXW.KEY
 call :MakeLink %USERPROFILE%\afxw64_163\AFXW.KEY.TXT %~dp0config\AFXW.KEY.TXT
+call :MakeLink %USERPROFILE%\afxw64_163\AFXW.INI %~dp0config\AFXW.INI
+call :MakeLink %USERPROFILE%\afxw64_163\AFXW.DEF %~dp0config\AFXW.DEF
 pause
 exit /b
 
